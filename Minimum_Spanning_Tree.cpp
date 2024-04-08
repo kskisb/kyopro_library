af24dc47f553a921d25e116bb7c2136f962624a4
kskisb <isbkosuke@gmail.com>
2024-04-08 19:09:45 +0900

    renamed and modified Minimum_Spanning_Tree.cpp

--- Minimum_Spanning_Tree.cpp
+++ Minimum_Spanning_Tree.cpp
@@ -1,15 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <algorithm>
 using namespace std;
 using ll = long long;
-using veci = vector<int>;
-using vecll = vector<ll>;
-using vecd = vector<double>;
-using vveci = vector<veci>;
-using vvecll = vector<vecll>;
-using vvecd = vector<vecd>;
-using Graph = vector<vector<int>>;
-#define rep(i, l, r) for(int i=l; i<r; i++)
-#define rrep(i, l, r) for(int i=r-1; i>=l; i--)
+ll INF = 1LL << 60;
 
 // 頂点 N, 辺の数 M
 int N, M;
@@ -17,13 +14,13 @@ int N, M;
 class UnionFind {
 public:
     // 親の番号を格納
-    veci parent;
+    vector<int> parent;
     // 自分がいるグループの辺の数を格納
-    veci sides;
+    vector<int> sides;
 
     UnionFind(int N) {
-        parent = veci(N, -1);
-        sides = veci(N, 0);
+        parent = vector<int>(N, -1);
+        sides = vector<int>(N, 0);
     }
 
     // 頂点 A のグループの 根 を返す
@@ -51,7 +48,7 @@ public:
         parent[A] += parent[B];
         parent[B] = A;
         sides[A]++;
-        
+
         return true;
     }
 };
@@ -61,17 +58,19 @@ int main() {
     UnionFind UN(N);
 
     vector<pair<int , pair<int, int>>> weighted_edge(M);
-    rep(i, 0, M) {
-        int a, b, c; cin >> a >> b >> c;
+    for(int i=0; i<M; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
         weighted_edge[i] = make_pair(c, make_pair(a, b));
+        weighted_edge[i] = make_pair(c, make_pair(b, a));
     }
 
     sort(weighted_edge.begin(), weighted_edge.end());
 
     ll ans = 0; // MST の辺の重みの総和
-    rep(i, 0, M) {  // ここで rrep にすると 最大全域木 になる
-        int u = weighted_edge[i].second.first - 1;
-        int v = weighted_edge[i].second.second - 1;
+    for(int i=0; i<M; i++) {  // ここで 逆順(i: M-1 -> 0) にすると 最大全域木 になる
+        int u = weighted_edge[i].second.first;
+        int v = weighted_edge[i].second.second;
         if(UN.root(u) == UN.root(v)) continue;
         else {
             ans += weighted_edge[i].first;
